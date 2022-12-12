695361342a44de5f6b0ac4d2fcf7ddde11919e1c
João Henrique Almeida Xavier <63925427+jhenriqueax@users.noreply.github.com>
2022-12-12 17:55:52 -0300

    refactor

---
+++ Intenções.py
@@ -0,0 +1,13 @@
+from treinamento import treinar
+
+R1 = "Você encontrarar essa informação na secretária"
+P1 = ["Preciso do meu histórico", "Onde consigo meu histórico", "Quero meu Histórico"]
+
+treinar(R1, P1)
+
+
+
+R2 = "funcionou"
+P2 = ["Preciso do meu RDM", "Onde consigo a minha declaração de RDM", "Quero meu RDM", "RDM", "rdm"]
+
+treinar(R2, P2)
